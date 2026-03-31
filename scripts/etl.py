cb66c1b451a785d872a87247e53699d4af5c6682
Jonatas Chaves <soaresjonatas755@gmail.com>
2026-03-31 12:18:42 -0300

    Update etl.py

--- scripts/etl.py
+++ scripts/etl.py
@@ -5,10 +5,8 @@ os.makedirs("data", exist_ok=True)
 
 df = pd.read_csv("data/transacoes.csv")
 
-# limpeza
 df.dropna(inplace=True)
 
-# transformação
 df["valor"] = df["valor"].astype(float)
 
 df.to_csv("data/transacoes_tratadas.csv", index=False)
